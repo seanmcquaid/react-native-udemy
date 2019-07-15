e5dd40c63e803c24968f313633f5b27e51062f5d
seanmcquaid <sean.mcquaid2@verizon.net>
2019-07-14 23:55:43 -0400

    seperated out some JSX into components and getting better concepts on how to structure reusable components

--- App.js
+++ App.js
@@ -1,52 +1,26 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
-
-import ListItem from './src/components/ListItem/ListItem';
+import { StyleSheet, View} from "react-native";
+import PlaceInput from "./src/components/PlaceInput/PlaceInput";
+import PlaceList from "./src/components/PlaceList/PlaceList";
 
 class App extends Component {
   state = {
-    placeName: "",
     places: []
   };
 
-  placeNameChangedHandler = val => {
-    this.setState({
-      placeName: val
-    });
-  };
-
-  placeSubmitHandler = () => {
-    if (this.state.placeName.trim() === "") {
-      return;
-    }
-
+  placeAddedHandler = placeName => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(prevState.placeName)
+        places: prevState.places.concat(placeName)
       };
     });
   };
 
   render() {
-    const placesOutput = this.state.places.map((place, i) => (
-      <ListItem key={i} placeName={place} />
-    ));
     return (
       <View style={styles.container}>
-        <View style={styles.inputContainer}>
-          <TextInput
-            placeholder="An awesome place"
-            value={this.state.placeName}
-            onChangeText={this.placeNameChangedHandler}
-            style={styles.placeInput}
-          />
-          <Button
-            title="Add"
-            style={styles.placeButton}
-            onPress={this.placeSubmitHandler}
-          />
-        </View>
-        <View style={styles.listContainer}>{placesOutput}</View>
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList places={this.state.places}/>
       </View>
     );
   }
@@ -60,22 +34,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "flex-start"
   },
-  inputContainer: {
-    // flex: 1,
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center"
-  },
-  placeInput: {
-    width: "70%"
-  },
-  placeButton: {
-    width: "30%"
-  },
-  listContainer: {
-    width: "100%"
-  }
 });
 
 export default App;
---
+++ src/components/PlaceList/PlaceList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {View, StyleSheet} from "react-native";
+import ListItem from "../ListItem/ListItem";
+
+const PlaceList = props => {
+    const placesOutput = props.places.map((place, i) => (
+        <ListItem key={i} placeName={place} />
+    ));
+    return (
+        <View style={styles.listContainer}>
+            {placesOutput}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    listContainer: {
+        width: "100%"
+    }
+});
+
+export default PlaceList;
