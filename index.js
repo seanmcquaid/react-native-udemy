0b68e74acabbc0e6b39b124d95df81abd0fb39e1
seanmcquaid <sean.mcquaid2@verizon.net>
2019-07-20 23:15:23 -0400

    configured store

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 /**
  * @format
  */
-
+import React from "react";
 import {AppRegistry} from 'react-native';
+import {Provider} from "react-redux";
+import configureStore from "./src/store/configureStore";
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const store = configureStore();
+
+const RNRedux = () => (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => RNRedux);
