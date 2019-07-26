de93835843ef533e2e2661f885de3c05b73146f3
seanmcquaid <sean.mcquaid2@verizon.net>
2019-07-25 23:01:28 -0400

    going to finish this project out to learn from max but will need to use react navigation instead of react-native navigation

---
+++ src/screens/Auth/Auth.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class AuthScreen extends Component {
+    render(){
+        return(
+            <View>
+                <Text>Auth Screen</Text>
+            </View>
+        )
+    }
+}
+
+export default AuthScreen;
