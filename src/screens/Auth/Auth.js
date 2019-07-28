a449c3e6a44c8f82ddb8a46e3c904448c6ed639f
seanmcquaid <sean.mcquaid2@verizon.net>
2019-07-27 22:57:37 -0400

    added flexbox, same as normal css

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
 
@@ -10,12 +10,24 @@ class AuthScreen extends Component {
 
     render () {
         return (
-            <View>
-                <Text>Auth Screen</Text>
+            <View style={styles.container}>
+                <Text>Please Login></Text>
+                <Button title="Switch To Login"/>
+                <TextInput placeholder="Your E-Mail Address"/>
+                <TextInput placeholder="Password"/>
+                <TextInput placeholder="Confirm Password"/>
                 <Button title="Login" onPress={this.loginHandler}/>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container : {
+        flex : 1,
+        justifyContent : "center",
+        alignItems : "center"
+    }
+})
 
 export default AuthScreen;
