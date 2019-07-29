316e834e6084b7381e289572c61db242d175c065
seanmcquaid <sean.mcquaid2@verizon.net>
2019-07-29 17:00:42 -0400

    Platform API is definitely why I got into react native, how cool

--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -3,7 +3,7 @@ import {
   View,
   Image,
   Text,
-  Button,
+  Platform,
   StyleSheet,
   TouchableOpacity
 } from "react-native";
@@ -28,7 +28,11 @@ class PlaceDetail extends Component {
         <View>
           <TouchableOpacity onPress={this.placeDeletedHandler}>
             <View style={styles.deleteButton}>
-              <Icon size={30} name="ios-trash" color="red" />
+              <Icon 
+                size={30} 
+                name={Platform.OS === "android" ? "md-trash" : "ios-trash"} 
+                color="red" 
+              />
             </View>
           </TouchableOpacity>
         </View>
