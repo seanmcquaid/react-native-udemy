7822a1ae9dfafed61c52bd3593095dae1d05522d
seanmcquaid <sean.mcquaid2@verizon.net>
2019-07-29 16:49:26 -0400

    platform api is cool git push

--- src/components/UI/ButtonWithBackground/ButtonWithBackground.js
+++ src/components/UI/ButtonWithBackground/ButtonWithBackground.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { TouchableOpacity, Text, View, StyleSheet } from 'react-native';
+import { 
+    TouchableOpacity, 
+    TouchableNativeFeedback, 
+    Text, 
+    View, 
+    StyleSheet, 
+    Platform 
+} from 'react-native';
 
-const buttonWithBackground = props => (
-    <TouchableOpacity onPress={props.onPress}>
+const buttonWithBackground = props => {
+    const content = (
         <View style={[styles.button, {backgroundColor: props.color}]}>
-            <Text>{props.children}</Text>
+                <Text>{props.children}</Text>
         </View>
-    </TouchableOpacity>
-);
+    );
+    if(Platform.OS === "android"){
+        return(
+            <TouchableNativeFeedback onPress={props.onPress}>
+                {content}
+            </TouchableNativeFeedback>
+        );
+    }
+    return (
+        <TouchableOpacity onPress={props.onPress}>
+            {content}
+        </TouchableOpacity>
+    )
+}
 
 const styles = StyleSheet.create({
     button: {
