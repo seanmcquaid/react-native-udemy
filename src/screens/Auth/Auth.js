69ba2cfa9ea2a782c0cd562ad1c3829280d10eaf
seanmcquaid <sean.mcquaid2@verizon.net>
2019-07-31 22:36:56 -0400

    custom validation added, good to learn this finally

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -15,10 +15,34 @@ import HeadingText from "../../components/UI/HeadingText/HeadingText";
 import MainText from "../../components/UI/MainText/MainText";
 import ButtonWithBackground from "../../components/UI/ButtonWithBackground/ButtonWithBackground";
 import backgroundImage from "../../assets/background.jpg";
+import validate from "../../utility/validation";
 
 class AuthScreen extends Component {
   state = {
-    viewMode: Dimensions.get("window").height > 500 ? "portrait" : "landscape"
+    viewMode: Dimensions.get("window").height > 500 ? "portrait" : "landscape",
+    controls: {
+      email: {
+        value: "",
+        valid: false,
+        validationRules: {
+          isEmail: true
+        }
+      },
+      password: {
+        value: "",
+        valid: false,
+        validationRules: {
+          minLength: 6
+        }
+      },
+      confirmPassword: {
+        value: "",
+        valid: false,
+        validationRules: {
+          equalTo: "password"
+        }
+      }
+    }
   };
 
   constructor(props) {
@@ -30,15 +54,59 @@ class AuthScreen extends Component {
     Dimensions.removeEventListener("change", this.updateStyles);
   }
 
-  updateStyles = (dims) => {
+  updateStyles = dims => {
     this.setState({
-      viewMode:
-        dims.window.height > 500 ? "portrait" : "landscape"
+      viewMode: dims.window.height > 500 ? "portrait" : "landscape"
     });
-  }
+  };
 
   loginHandler = () => {
     startMainTabs();
+  };
+
+  updateInputState = (key, value) => {
+    let connectedValue = {};
+    if (this.state.controls[key].validationRules.equalTo) {
+      const equalControl = this.state.controls[key].validationRules.equalTo;
+      const equalValue = this.state.controls[equalControl].value;
+      connectedValue = {
+        ...connectedValue,
+        equalTo: equalValue
+      };
+    }
+    if (key === "password") {
+      connectedValue = {
+        ...connectedValue,
+        equalTo: value
+      };
+    }
+    this.setState(prevState => {
+      return {
+        controls: {
+          ...prevState.controls,
+          confirmPassword: {
+            ...prevState.controls.confirmPassword,
+            valid:
+              key === "password"
+                ? validate(
+                    prevState.controls.confirmPassword.value,
+                    prevState.controls.confirmPassword.validationRules,
+                    connectedValue
+                  )
+                : prevState.controls.confirmPassword.valid
+          },
+          [key]: {
+            ...prevState.controls[key],
+            value: value,
+            valid: validate(
+              value,
+              prevState.controls[key].validationRules,
+              connectedValue
+            )
+          }
+        }
+      };
+    });
   };
 
   render() {
@@ -62,6 +130,8 @@ class AuthScreen extends Component {
             <DefaultInput
               placeholder="Your E-Mail Address"
               style={styles.input}
+              value={this.state.controls.email.value}
+              onChangeText={val => this.updateInputState("email", val)}
             />
             <View
               style={
@@ -77,7 +147,12 @@ class AuthScreen extends Component {
                     : styles.landscapePasswordWrapper
                 }
               >
-                <DefaultInput placeholder="Password" style={styles.input} />
+                <DefaultInput
+                  placeholder="Password"
+                  style={styles.input}
+                  value={this.state.controls.password.value}
+                  onChangeText={val => this.updateInputState("password", val)}
+                />
               </View>
               <View
                 style={
@@ -89,6 +164,9 @@ class AuthScreen extends Component {
                 <DefaultInput
                   placeholder="Confirm Password"
                   style={styles.input}
+                  value={this.state.controls.confirmPassword.value}
+                  onChangeText={val =>
+                    this.updateInputState("confirmPassword", val)}
                 />
               </View>
             </View>
