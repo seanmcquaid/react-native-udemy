bb940a5791e755d2f3db196390a5c20d82cb362d
seanmcquaid <sean.mcquaid2@verizon.net>
2019-08-01 21:16:19 -0400

    utilized valid state

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -26,21 +26,24 @@ class AuthScreen extends Component {
         valid: false,
         validationRules: {
           isEmail: true
-        }
+        },
+        touched: false
       },
       password: {
         value: "",
         valid: false,
         validationRules: {
           minLength: 6
-        }
+        },
+        touched: false
       },
       confirmPassword: {
         value: "",
         valid: false,
         validationRules: {
           equalTo: "password"
-        }
+        },
+        touched: false
       }
     }
   };
@@ -102,7 +105,8 @@ class AuthScreen extends Component {
               value,
               prevState.controls[key].validationRules,
               connectedValue
-            )
+            ),
+            touched: true
           }
         }
       };
@@ -132,6 +136,8 @@ class AuthScreen extends Component {
               style={styles.input}
               value={this.state.controls.email.value}
               onChangeText={val => this.updateInputState("email", val)}
+              valid={this.state.controls.email.valid}
+              touched={this.state.controls.email.touched}
             />
             <View
               style={
@@ -152,6 +158,8 @@ class AuthScreen extends Component {
                   style={styles.input}
                   value={this.state.controls.password.value}
                   onChangeText={val => this.updateInputState("password", val)}
+                  valid={this.state.controls.password.valid}
+                  touched={this.state.controls.password.touched}
                 />
               </View>
               <View
@@ -167,11 +175,21 @@ class AuthScreen extends Component {
                   value={this.state.controls.confirmPassword.value}
                   onChangeText={val =>
                     this.updateInputState("confirmPassword", val)}
+                  valid={this.state.controls.confirmPassword.valid}
+                  touched={this.state.controls.confirmPassword.touched}
                 />
               </View>
             </View>
           </View>
-          <ButtonWithBackground color="#29aaf4" onPress={this.loginHandler}>
+          <ButtonWithBackground
+            color="#29aaf4"
+            onPress={this.loginHandler}
+            disabled={
+              !this.state.controls.confirmPassword.valid ||
+              !this.state.controls.email.valid ||
+              !this.state.controls.password.valid
+            }
+          >
             Submit
           </ButtonWithBackground>
         </View>
