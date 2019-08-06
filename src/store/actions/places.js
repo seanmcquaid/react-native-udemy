025e8915c3562539939966492e3caed1ed565a89
seanmcquaid <sean.mcquaid2@verizon.net>
2019-08-05 20:28:52 -0400

    learned about firebase functions

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -2,14 +2,25 @@ import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName, location, image) => {
     return dispatch => {
-        const placeData = {
-            name: placeName,
-            location: location
-        };
-        fetch("https://awesome-places-1511248766522.firebaseio.com/places.json", {
+        fetch("https://us-central1-awesome-places-1511248766522.cloudfunctions.net/storeImage", {
             method: "POST",
-            body: JSON.stringify(placeData)
+            body: JSON.stringify({
+                image: image.base64
+            })
         })
+        .catch(err => console.log(err))
+        .then(res => res.json())
+        .then(parsedRes => {
+            const placeData = {
+                name: placeName,
+                location: location,
+                image: parsedRes.imageUrl
+            };
+            return fetch("https://awesome-places-1511248766522.firebaseio.com/places.json", {
+                method: "POST",
+                body: JSON.stringify(placeData)
+            })
+        })  
         .catch(err => console.log(err))
         .then(res => res.json())
         .then(parsedRes => {
