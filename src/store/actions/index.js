09392911eb2c86d0579b45a7e3eb8692268d2431
seanmcquaid <sean.mcquaid2@verizon.net>
2019-08-08 19:39:19 -0400

    learned about async storage and implemented auto sign in

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,3 @@
 export { addPlace, deletePlace, getPlaces } from "./places";
-export { tryAuth, authGetToken } from "./auth";
+export { tryAuth } from "./auth";
 export { uiStartLoading, uiStopLoading } from "./ui";
